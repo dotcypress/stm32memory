a3b0ecffe0726b7a756c7e8a9db7303d79344881
Vitaly Domnikov <dotcypress@gmail.com>
2022-11-18 14:49:06 -0800

    0.0.1

--- pages/api/[mcu].js
+++ pages/api/[mcu].js
@@ -1,16 +1,20 @@
 import db from './stm32';
 
 export default function handler(req, res) {
-  let mcu = db.find((item) => item.CommercialPartNo == req.query.mcu.toUpperCase() || item.Reference == req.query.mcu.toUpperCase());
+  let name = req.query.mcu.toUpperCase();
+  let mcu = db.find((item) => item.CommercialPartNo == name || item.Reference == name);
   if (!mcu) {
     return res.status(404);
   }
   let flash = mcu.Flash.replace(" kBytes", "K");
   let ram = mcu.RAM.replace(" kBytes", "K");
-  res.status(200).send(`/* Linker script for the ${mcu.Reference} */
+  res.setHeader("Content-Disposition", "attachment; filename=\"memory.x\"")
+  res.status(200).send(
+    `/* Linker script for the ${mcu.Reference} */
 MEMORY
 {
   FLASH : ORIGIN = 0x08000000, LENGTH = ${flash}
-  RAM :   ORIGIN = 0x20000000, LENGTH = ${ram}
-}`);
+  RAM   : ORIGIN = 0x20000000, LENGTH = ${ram}
+}
+`);
 }
--- pages/index.js
+++ pages/index.js
@@ -6,43 +6,44 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>STM32 memory.x generator</title>
-        <meta name="description" content="Generate memory.x for your STM32 Rust project" />
+        <title>🦀 STM32 memory.x generator</title>
+        <meta name="description" content="🦀 Generate memory.x for your STM32 Rust project" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          🦀
-        </h1>
-        <h1 className={styles.title}>
+        <h1 className={styles.title}>🦀</h1>
+        <h1 className={styles.subtitle}>
           STM32 memory.x generator
         </h1>
 
-        <p className={styles.description}>
+        <p className={styles.usage}>
           <code className={styles.code}>curl https://stm32.vercel.app/api/%chip% &gt; memory.x</code>
         </p>
 
-        <p className={styles.examples}>
-          Examples:
+        <p className={styles.help}>
+          List supported devices:
+          <ul>
+            <li><code className={styles.code}>curl https://stm32.vercel.app/api/db &#124; jq</code></li>
+          </ul>
+
+          Usage examples:
           <ul>
             <li><code className={styles.code}>curl https://stm32.vercel.app/api/stm32g030k8t6 &gt; memory.x</code></li>
             <li><code className={styles.code}>curl https://stm32.vercel.app/api/stm32g031k8tx &gt; memory.x</code></li>
             <li><code className={styles.code}>curl https://stm32.vercel.app/api/stm32f103cbu6 &gt; memory.x</code></li>
           </ul>
-
-          Supported devices:
-          <ul>
-            <li><code className={styles.code}>curl https://stm32.vercel.app/api/db &#124; jq</code></li>
-          </ul>
-        </p>
-
-        <p className={styles.examples}>
-         
         </p>
       </main>
 
       <footer className={styles.footer}>
+        <a
+          href="https://github.com/dotcypress/stm32memory"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Fork me on GitHub
+        </a>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
