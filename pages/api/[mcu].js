c4dc1f1720b77a55a8968bc34420bf4d3bd6047f
Vitaly Domnikov <dotcypress@gmail.com>
2022-11-18 15:18:23 -0800

    cleanup

--- pages/api/[mcu].js
+++ pages/api/[mcu].js
@@ -4,11 +4,11 @@ export default function handler(req, res) {
   let name = req.query.mcu.toUpperCase();
   let mcu = db.find((item) => item.CommercialPartNo == name || item.Reference == name);
   if (!mcu) {
-    return res.status(404);
+    return res.status(404).send();
   }
   let flash = mcu.Flash.replace(" kBytes", "K");
   let ram = mcu.RAM.replace(" kBytes", "K");
-  res.setHeader("Content-Disposition", "attachment; filename=\"memory.x\"")
+  res.setHeader("Content-Disposition", `attachment; filename="memory.x"`)
   res.status(200).send(
     `/* Linker script for the ${mcu.Reference} */
 MEMORY
